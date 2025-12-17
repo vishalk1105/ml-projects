acf99fe9912ef2b051352e72598fcc11eed42afb
vishal vilas kamble <vishalvkamble95@gmail.com>
2025-12-17 21:19:12 +0530

    Data Transformation is added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,13 +6,13 @@ from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path : str = os.path.join('artifacts',"train.csv")
     test_data_path : str = os.path.join('artifacts',"test.csv")
     raw_data_path : str = os.path.join('artifacts',"raw.csv")
-
 
 class DataIngestion:
     def __init__(self):
@@ -46,5 +46,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj= DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data=obj.initiate_data_ingestion()
     
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
